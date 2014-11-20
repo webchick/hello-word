ea2843ae3c23cb1f94db41a6b11b8b90d42eb4db
webchick <drupal@webchick.net>
2014-11-20 10:39:02 -0800

    Updating to more current header requirements per https://developer.wordpress.org/plugins/the-basics/header-requirements/

--- hello-word.php
+++ hello-word.php
@@ -1,13 +1,16 @@
 <?php
-/**
- * Plugin Name: Hello Word
- * Plugin URI: http://hello.example.com/
- * Description: "Hello World" in a WordPress plugin implementation.
- * Version: 1.0.0
- * Author: webchick
- * Author URI: http://webchick.net/
- * License: GPL2
- */
+/*
+Plugin Name: Hello Word
+Plugin URI: http://hello.example.com/
+Description: "Hello World" in a WordPress plugin.
+Version: 1.0.0
+Author: webchick
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: hello-word
+*/
 
 defined('ABSPATH') or die("No script kiddies please!");
 

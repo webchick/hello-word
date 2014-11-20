131412b3c6dae3c5aa1b242a7ad1c825851dee8c
webchick <drupal@webchick.net>
2014-11-20 10:41:48 -0800

    Adding license blurb per https://developer.wordpress.org/plugins/the-basics/including-a-software-license/

--- hello-word.php
+++ hello-word.php
@@ -10,6 +10,20 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
 Text Domain: hello-word
+
+Hello Word is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Hello Word is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hello World. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+
 */
 
 defined('ABSPATH') or die("No script kiddies please!");
